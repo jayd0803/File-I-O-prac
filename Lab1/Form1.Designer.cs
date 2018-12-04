c96a33b4ca7bf490fdb06a9529a9760a902fa1e3
jayd0803 <jayd0803@gmail.com>
2018-12-04 12:53:00 -0500

    Initial commit

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAgent = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtShowing = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Showing Tracker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Agent ID";
+            // 
+            // txtAgent
+            // 
+            this.txtAgent.Location = new System.Drawing.Point(149, 78);
+            this.txtAgent.Name = "txtAgent";
+            this.txtAgent.Size = new System.Drawing.Size(63, 20);
+            this.txtAgent.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(235, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "( 1 - 7 )";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(85, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Month";
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(149, 115);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(63, 20);
+            this.txtMonth.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(235, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "( 1 - 12 )";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Number of Showings";
+            // 
+            // txtShowing
+            // 
+            this.txtShowing.Location = new System.Drawing.Point(149, 157);
+            this.txtShowing.Name = "txtShowing";
+            this.txtShowing.Size = new System.Drawing.Size(119, 20);
+            this.txtShowing.TabIndex = 8;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(68, 207);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(166, 207);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Location = new System.Drawing.Point(34, 251);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 71);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(34, 28);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save to File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(132, 28);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(95, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear All Data";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtShowing);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAgent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "House Pros";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAgent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtShowing;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        List<int[]> data = new List<int[]>();
+        string[] months = new string[] {"January","February","March","April","May","June","July","August","September",
+                "Ocotober","November","December"
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 7; i++)
+            {
+                data.Add(new int[12]);
+            }
+
+        }
+
+        private void clearTxts()
+        {
+            txtAgent.Clear();
+            txtMonth.Clear();
+            txtShowing.Clear();
+        }
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            clearTxts();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = "";
+
+                int agentId;
+                int.TryParse(txtAgent.Text, out agentId);
+                if (agentId < 1 || agentId > 7)
+                {
+                    msg += "Please enter a valid Agent ID" + Environment.NewLine;
+                    txtAgent.Clear();
+                }
+                int month;
+                int.TryParse(txtMonth.Text, out month);
+                if (month < 1 || month > 12)
+                {
+                    msg += "Please enter a valid Month" + Environment.NewLine;
+                    txtMonth.Clear();
+                }
+                int showing;
+                int.TryParse(txtShowing.Text, out showing);
+                if (showing == 0)
+                {
+                    msg += "Please enter a valid number of showings" + Environment.NewLine;
+                    txtShowing.Clear();
+                }
+
+                if(msg == "")
+                {
+                    data[agentId - 1][month - 1] = showing;
+                    clearTxts();
+                }
+
+                MessageBox.Show(msg == "" ? "Saved: " + showing + " showings by Agent #" + agentId + " in " + months[month - 1] : msg);
+
+            }
+            catch
+            {
+                MessageBox.Show("Cannot Insert new record, please contact Admin for help");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            data.Clear();
+            for (int i = 0; i < 7; i++)
+            {
+                data.Add(new int[12]);
+            }
+            clearTxts();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "txt files (*.txt)|*.txt";
+                sfd.DefaultExt = "txt";
+                sfd.ShowDialog();
+
+
+                FileStream fs = new FileStream(sfd.FileName, FileMode.Create);
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.Write("Showings Tracking Information ({0}){1}{2}", DateTime.Now.Date.ToShortDateString(),Environment.NewLine,Environment.NewLine );
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        sw.Write("Agent #{0}{1}",(i + 1),Environment.NewLine);
+                        for (int m = 0; m < data[i].Length; m++)
+                        {
+                            sw.Write("\t{0}: {1}{2}", months[m],data[i][m], Environment.NewLine);
+                        }
+                        sw.Write(Environment.NewLine);
+
+                    }
+
+                }
+                fs.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Cannot save the file, please contact the Admin for help");
+            }
+        }
+    }
+}
